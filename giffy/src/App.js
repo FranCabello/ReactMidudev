88ebbfbeecc7888eef3eb83c59e1ceb0dcfc6aa9
Fran Cabello <fcabello@dataseekers.es>
2023-06-14 13:07:01 +0200

    01-03-midudev

--- giffy/src/App.js
+++ giffy/src/App.js
@@ -1,15 +1,42 @@
-import React, { useState } from "react"
+import React from "react"
 import "./App.css"
-import ListOfGifs from "./components/ListOfGifs"
+import Home from "./pages/Home"
+import SearchResults from "./pages/SearchResults"
+import Detail from "./pages/Detail"
+import StaticContext from "./context/StaticContext"
+import {GifsContextProvider} from "./context/GifsContext"
+
+import {Link, Route} from "wouter"
 
 export default function App() {
-  const [keyword, setKeyword] = useState('panda')
- 
   return (
-    <div className="App">
-      <section className="App-content">
-        <ListOfGifs keyword="panda" />
-      </section>
-    </div>
+    <StaticContext.Provider value={
+      {
+        name: "midudev", 
+        suscribeteAlCanal: true
+      }
+    }>
+        <div className="App">
+          <section className="App-content">
+            <Link to="/">
+              <img className="App-logo" alt="Giffy logo" src="/logo.png" />
+            </Link>
+            <GifsContextProvider>
+              <Route 
+                component={Home} 
+                path="/" 
+              />
+              <Route 
+                component={SearchResults} 
+                path="/search/:keyword" 
+              />
+              <Route 
+                component={Detail} 
+                path="/gif/:id" 
+              />
+            </GifsContextProvider>
+          </section>
+        </div>
+    </StaticContext.Provider>
   )
 }
--- giffy/src/services/getGifs.js
+++ giffy/src/services/getGifs.js
@@ -1,16 +1,16 @@
 const apiKey = "5zxgW2e35TmNqjI3RX8f5X6RJKyyARf0"
 
-export default function getGifts({ keyword = "morty" } = {}) {
+export default function getGifs({ keyword = "morty" } = {}) {
   const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
 
   return fetch(apiUrl)
     .then((res) => res.json())
     .then((response) => {
-      const { data = [] } = response
+      const {data = []} = response
       if (Array.isArray(data)) {
         const gifs = data.map((image) => {
-          const { images, title, id } = image
-          const { url } = image.images.fixed_width.url
+          const {images, title, id} = image
+          const { url } = images.fixed_width
           return { title, id, url }
         })
         return gifs
