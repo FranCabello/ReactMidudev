624891ba013b2f3ef4cccd77c4e79d7e1b16afcc
greatmela <fran.mlg.93@gmail.com>
2023-06-14 02:33:46 +0200

    gif API

---
+++ giffy/src/services/getGifs.js
@@ -0,0 +1,19 @@
+const apiKey = "5zxgW2e35TmNqjI3RX8f5X6RJKyyARf0"
+
+export default function getGifts({ keyword = "morty" } = {}) {
+  const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+
+  return fetch(apiUrl)
+    .then((res) => res.json())
+    .then((response) => {
+      const { data = [] } = response
+      if (Array.isArray(data)) {
+        const gifs = data.map((image) => {
+          const { images, title, id } = image
+          const { url } = image.images.fixed_width.url
+          return { title, id, url }
+        })
+        return gifs
+      }
+    })
+}
