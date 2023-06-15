72217ab7abf1d4f104e87d585976e3aee6c8da3c
Fran Cabello <fcabello@dataseekers.es>
2023-06-15 09:30:09 +0200

    04 done

--- giffy/src/hooks/useGifs.js
+++ giffy/src/hooks/useGifs.js
@@ -2,8 +2,12 @@ import {useContext, useEffect, useState} from "react"
 import getGifs from "../services/getGifs"
 import GifsContext from '../context/GifsContext'
 
+const INITIAL_PAGE = 0
+
 export function useGifs({ keyword } = { keyword: null }) {
     const [loading, setLoading] = useState(false)
+    const [loadingNextPage, setLoadingNextPage] = useState(false)
+    const [page, setPage] = useState(INITIAL_PAGE)
     const [gifs, setGifs] = useContext(GifsContext)
 
     // recuperamos la keyword del localStorage
@@ -21,5 +25,17 @@ export function useGifs({ keyword } = { keyword: null }) {
         })
     }, [keyword, keywordToUse, setGifs])
 
-    return {loading, gifs}
+    useEffect(function() {
+        if (page === INITIAL_PAGE) return
+
+        setLoadingNextPage(true)
+
+        getGifs({ keyword: keywordToUse, page })
+            .then(nextGifs => {
+                setGifs(prevGifs => prevGifs.concat(nextGifs))
+                setLoadingNextPage(false)
+            })
+    }, [keywordToUse, page, setGifs])
+
+    return {loading, loadingNextPage, gifs, setPage}
 }
