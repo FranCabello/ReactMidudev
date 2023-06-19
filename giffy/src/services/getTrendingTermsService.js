a7f49cce0b7c6ad4a116ef18b767abe660328ee5
Fran Cabello <fcabello@dataseekers.es>
2023-06-19 15:00:35 +0200

    19:45 SEO

--- giffy/src/services/getTrendingTermsService.js
+++ giffy/src/services/getTrendingTermsService.js
@@ -5,10 +5,10 @@ const fromApiResponseToGifs = apiResponse => {
   return data
 }
 
-export default function getTrendingTerms() {
+export default function getTrendingTerms({signal}) {
   const apiUrl = `${API_URL}/trending/searches?api_key=${API_KEY}`
 
-  return fetch(apiUrl)
+  return fetch(apiUrl, {signal})
     .then(res => res.json())
     .then(fromApiResponseToGifs)
 }
