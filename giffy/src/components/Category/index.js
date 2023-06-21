b4efce088067a486eba80e75552e4a7a18d33f62
Fran Cabello <fcabello@dataseekers.es>
2023-06-21 12:40:48 +0200

    css

--- giffy/src/components/Category/index.js
+++ giffy/src/components/Category/index.js
@@ -1,23 +1,21 @@
-import React from "react"
-import { Link } from "wouter"
+import React from "react";
+import { Link } from "wouter";
 
-import "./Category.css"
+import "./Category.css";
 
 export default function Category({ name, options = [] }) {
-    return (
-        <div className="Category">
-            <h3 className="Category-title">{decodeURI(name)}</h3>
-            <ul className="Category-list">
-                {options.map((singleOption) => (
-                    <li key={singleOption}>
-                        <Link 
-                            className="Category-link" 
-                            to={`/search/${singleOption}`}>
-                                {singleOption}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+  return (
+    <div className="Category">
+      <h3 className="Category-title">{decodeURI(name)}</h3>
+      <ul className="Category-list">
+        {options.map((singleOption) => (
+          <li key={singleOption}>
+            <Link className="Category-link" to={`/search/${singleOption}`}>
+              {singleOption}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
